d4a74482b6df610cf5312693019c5e0dfea9b947
guoguo <2012806421@qq.com>
2026-05-28 11:09:12 +0800

    添加用户自定义轮播图片

---
+++ src/assets/splash/index.ts
@@ -0,0 +1,15 @@
+import food1 from './food1.jpg';
+import food2 from './food2.jpg';
+import food3 from './food3.jpg';
+import food4 from './food4.jpg';
+import food5 from './food5.jpg';
+
+export const splashImages = [food1, food2, food3, food4, food5];
+
+export const imageFileNames = [
+  'food1.jpg',
+  'food2.jpg',
+  'food3.jpg',
+  'food4.jpg',
+  'food5.jpg',
+];
